51339d0cc9e878d83f4d6bea9b84eb2c98e714a8
khumayunkh <hasanovhumayun15@gmail.com>
2022-07-17 23:37:18 +0500

    articles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import AddArticles from "./components/AddArticls/Add"
+import Articles from "./components/Articles/Articles"
 import Header from "./components/Header/Header"
 import LoginForm from "./components/Login/Login"
 import Profile from "./components/Profile/Profile"
@@ -24,6 +25,10 @@ function App() {
       <Route
         path="/users"
         element={<Users/>}
+      />
+      <Route
+        path="/articles"
+        element={<Articles/>}
       />
       <Route
         path="/add"
---
+++ src/components/Articles/Articles.js
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './articles.module.css'
+
+
+function Articles(){
+    return(
+        <>
+        <div className={style.articles}>
+            <div className={style.container}>
+                <div className={style.articles_in}>
+                    
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+
+export default Articles
