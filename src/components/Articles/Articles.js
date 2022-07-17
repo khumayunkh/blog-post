fd97cd45133a1ac1f641dab21730bf2993028f73
khumayunkh <hasanovhumayun15@gmail.com>
2022-07-17 23:52:03 +0500

    articles

--- src/components/Articles/Articles.js
+++ src/components/Articles/Articles.js
@@ -8,7 +8,14 @@ function Articles(){
         <div className={style.articles}>
             <div className={style.container}>
                 <div className={style.articles_in}>
-                    
+                    <div className={style.article}>
+                        <div className={style.header}>
+                            <img src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png"/>
+                            <h4>Humoyun</h4>
+                        </div>
+                        <h2>Why Your Favorite Foods Taste So Good</h2>
+                        <h3>How do we decide what we like to eat?</h3>
+                    </div>
                 </div>
             </div>
         </div>
