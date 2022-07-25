859c7c2dcb3ed8bdd43a995b8c88fe13cb9f2703
khumayunkh <hasanovhumayun15@gmail.com>
2022-07-25 17:02:29 +0500

    login useForm

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import style from './login.module.css'
 import { useDispatch } from "react-redux";
 import { loginThunk } from "../../store/authSlice/authSlice";
 import { NavLink } from "react-router-dom";
+import {useForm} from 'react-hook-form'
 
 function LoginForm(){
+    const {register, handleSubmit} = useForm()
     const dispatch = useDispatch()
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const emailLogin = register("email", {required: true})
+    const passwordLogin = register("password", {required: true})
 
 
-    const handleSubmit = () =>{
-        dispatch(loginThunk(email,password))
-        setEmail('')
-        setPassword('')
+    const onSubmit = (data) =>{
+        dispatch(loginThunk({
+            email: data.email,
+            password : data.password
+        }))
     }
 
     
@@ -24,23 +27,23 @@ function LoginForm(){
         <div className={style.LoginForm}>
             <div className={style.LoginForm_in}>
                 <h1>Login</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
                 <input
                     className={style.input}
-                    onChange={e => setEmail(e.target.value)}
-                    value={email}
+                    {...emailLogin}
                     type="text"
                     placeholder='Email'
                 />
                 <input
                     className={style.input}
-                    onChange={e => setPassword(e.target.value)}
-                    value={password}
+                    {...passwordLogin}
                     type="password"
                     placeholder='Пароль'
                 />
-                <button className={style.btn_login} onClick={handleSubmit}>
-                    Логин
+                <button className={style.btn_login}>
+                    Login
                 </button>
+                </form>
                 <NavLink to='/registr' className={style.btn_sign_in}>
                     Регистрация
                 </NavLink>
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
 import style from './registration.module.css'
 import { registrationThunk } from "../../store/authSlice/authSlice";
