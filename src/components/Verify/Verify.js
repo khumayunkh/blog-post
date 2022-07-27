577dc06954f8217d8878fa52fcdd3ac737d1bfd2
khumayunkh <hasanovhumayun15@gmail.com>
2022-07-28 03:40:54 +0500

    style

--- src/components/Verify/Verify.js
+++ src/components/Verify/Verify.js
@@ -1,21 +1,16 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { verifyEmailThunk } from "../../store/authSlice/authSlice";
 
 
 function Verify(){
-
     const dispatch = useDispatch() 
     let {token} = useParams()
 
     useEffect( () => {
         dispatch(verifyEmailThunk(token))
     }, []);
-    
-    return(
-        <>hello {token}</>
-    )
 }
 
 export default Verify
