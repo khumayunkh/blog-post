445da47045dedbde7623fcb1a25722a6d4df3909
khumayunkh <hasanovhumayun15@gmail.com>
2022-07-29 21:36:05 +0500

    users

---
+++ src/api/users.js
@@ -0,0 +1,17 @@
+import {refreshToken} from "./auth";
+import {client} from "./index";
+
+
+export const getMe = async () => {
+    const refresh_token = localStorage.getItem('refresh_token')
+    try{
+        return await client.get('users/me/')
+    } catch(error){
+        if(localStorage.getItem('refresh_token')){
+            const response = await refreshToken({refresh: refresh_token})
+            localStorage.setItem('access_token', response.data.access)
+            localStorage.setItem('refresh_token', response.data.refresh)
+            return await client.get('users/me')
+        } else throw error
+    }
+}
