dcbc1d99aa5ec0407718aa302ec2603bc281abd7
khumayunkh <hasanovhumayun15@gmail.com>
2022-08-01 16:47:06 +0500

    addArticles

--- src/components/AddArticls/Add.js
+++ src/components/AddArticls/Add.js
@@ -1,16 +1,36 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { addNewArticlesThunk } from "../../store/articles/articlesReducer";
 import style from './add.module.css'
 
 function AddArticles(){
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+
+    const nameAddArticles = register('tags')
+    const titleAddArticles = register('title')
+    const contentAddArticles = register('content')
+    
+    const onSubmit = (data) =>{
+        dispatch(addNewArticlesThunk({
+            tags : data.tags,
+            title : data.title,
+            content : data.content
+        }))
+    }
+
     return(
         <>
         <div className={style.add}>
             <div className={style.container}>
                 <div className={style.add_in}>
-                    <div className={style.input}>
-                        <input placeholder="Tell your story..."/>
+                    <form onSubmit={handleSubmit(onSubmit)} className={style.input}>
+                        <input {...nameAddArticles} className={style.input_in} placeholder="Name"/>
+                        <input {...titleAddArticles} className={style.input_in} placeholder="Title"/>
+                        <input {...contentAddArticles} className={style.input_in} placeholder="Tell your story..."/>
                         <button>Publish</button>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
